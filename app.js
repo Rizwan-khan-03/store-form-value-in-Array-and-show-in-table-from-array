506616c04689fed8c5a1bc85d446ffce42602923
Rizwan khan <69905693+Rizwan-khan-03@users.noreply.github.com>
2022-02-21 11:55:53 +0530

    update

--- app.js
+++ app.js
@@ -37,47 +37,32 @@ var person = [
   
   
       ];
- localStorage.setItem( "function",table() );
- function table(){
-  const tableContainer = document.querySelector('.table-container');
-    const htmltable =document.createElement("table")
-    const tr = document.createElement("tr")
-    const th_1= document.createElement("th");
-    const th_2 = document.createElement("th");
-    const th_3 = document.createElement("th");
-    th_1.innerHTML='FIRST NAME'
-    th_2.innerHTML='LAST NAME'
-    th_3.innerHTML='CONTACT'
-    tr.appendChild(th_1);
-    tr.appendChild(th_2);
-    tr.appendChild(th_3);
-   
-    htmltable.setAttribute('id',"customers")
-    htmltable.appendChild(tr);
-    tableContainer.appendChild(htmltable);
-}
-//  localStorage.getItem(table())
+     
 
-
-
-      const customers_table = document.querySelector('#customers');
+  
       var myForm=document.getElementById('myForm');
-
       myForm.addEventListener('submit',function(event){
       event.preventDefault();
-      
-    
       var firstName=document.getElementById('fname').value;
       var lastName=document.getElementById('lname').value;
       var number=document.getElementById('contact').value;
-
+      if (firstName==null || firstName=='' ||firstName==number){
+        alert('Please Type Correct Name')
+      }
+      if (isNaN(number)){
+        alert('Enter Numeric Value only here')
+      }
       person.push({firstName:firstName,lastName:lastName,number:number})
-    
-      customers_table.innerHTML=(person.map(function (item) {
-      return `<tr><td>${item.firstName}</td><td>${item.lastName}</td><td>${item.number}</td></tr>`
-    
-    })).join('');
+      localStorage.setItem("array",person);
+      localStorage.getItem("ayyay",person.firstName)
+
+      const table = document.querySelector('#table-body');
   
+      table.innerHTML=(person.map(function (item) {
+         return `<tr><td>${item.firstName}</td><td>${item.lastName}</td><td>${item.number}</td></tr>`
+      
+       })).join('')
+    
 
     console.log(person)
     })
